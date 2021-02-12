ec68b9b328248cd7c3a9f479b528465c98266074
Serhat S <serhats@tutamail.com>
2021-02-12 03:30:13 +0300

    ✨ Add new methods

--- board.go
+++ board.go
@@ -36,6 +36,26 @@ func (board *Board) BoxHasNumber(boxColumn, boxRow, number int) bool {
 	return false
 }
 
+// IsGuessValid checks if guess not violates board rules
+func (board *Board) IsGuessValid(column, row int, guess int) bool {
+	boxColumn, boxRow := getBoxCoordinate(column, row)
+	return !(board.BoxHasNumber(boxColumn, boxRow, guess) ||
+		board.ColumnHasNumber(column, guess) ||
+		board.RowHasNumber(row, guess))
+}
+
+// GetNextEmptyCell gets first empty cell if exist
+func (board *Board) GetNextEmptyCell() (int, int, bool) {
+	for i, row := range board {
+		for j, num := range row {
+			if num == 0 {
+				return j, i, true
+			}
+		}
+	}
+	return 0, 0, false
+}
+
 func getBoxSlice(board Board, boxColumn, boxRow int) [][]int {
 	slice := [][]int{}
 	for i := 0; i < 3; i++ {
@@ -43,4 +63,8 @@ func getBoxSlice(board Board, boxColumn, boxRow int) [][]int {
 	}
 
 	return slice
+}
+
+func getBoxCoordinate(column, row int) (int, int) {
+	return (column / 3), (row / 3)
 }
--- board_test.go
+++ board_test.go
@@ -101,3 +101,123 @@ func TestBoxHasNumber(t *testing.T) {
 		}
 	}
 }
+
+func TestIsGuessValid(t *testing.T) {
+	tests := []struct {
+		column   int
+		row      int
+		guess    int
+		expected bool
+	}{
+		{1, 0, 2, true},
+		{1, 0, 4, false},
+		{0, 1, 3, false},
+		{0, 1, 9, false},
+		{3, 2, 1, false},
+		{3, 2, 2, true},
+		{3, 2, 3, false},
+		{3, 2, 4, false},
+		{3, 2, 5, false},
+		{3, 2, 6, false},
+		{3, 2, 7, false},
+		{3, 2, 8, false},
+		{3, 2, 9, false},
+		{7, 8, 2, false},
+		{7, 8, 4, true},
+	}
+
+	for _, test := range tests {
+		actual := board.IsGuessValid(test.column, test.row, test.guess)
+
+		if test.expected != actual {
+			t.Errorf("Test: (Guess %v in (%v, %v) is valid should return %v) failed!",
+				test.guess, test.column, test.row, test.expected)
+		}
+	}
+}
+
+func TestGetNextEmptyCell(t *testing.T) {
+	tests := []struct {
+		board         sudoku.Board
+		expectedCol   int
+		expectedRow   int
+		expectedFound bool
+	}{
+		{
+			board: sudoku.Board{
+				{1, 2, 3, 4, 5, 6, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 1, 2, 3, 4, 5, 6},
+				{3, 1, 2, 6, 4, 5, 9, 7, 8},
+				{5, 4, 6, 8, 7, 9, 3, 1, 2},
+				{7, 9, 8, 3, 1, 2, 6, 4, 5},
+				{2, 3, 1, 5, 0, 4, 8, 9, 7},
+				{4, 6, 5, 7, 9, 8, 2, 3, 1},
+				{8, 7, 9, 2, 3, 1, 5, 6, 4},
+			},
+			expectedCol:   4,
+			expectedRow:   6,
+			expectedFound: true,
+		},
+		{
+			board: sudoku.Board{
+				{1, 2, 3, 4, 5, 6, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 1, 2, 3, 4, 5, 6},
+				{3, 1, 2, 6, 4, 0, 9, 7, 8},
+				{5, 4, 6, 8, 7, 9, 3, 1, 2},
+				{7, 9, 8, 0, 1, 2, 6, 4, 5},
+				{2, 3, 1, 5, 6, 4, 8, 9, 7},
+				{4, 6, 5, 7, 9, 8, 2, 3, 1},
+				{8, 7, 9, 2, 3, 1, 5, 6, 4},
+			},
+			expectedCol:   5,
+			expectedRow:   3,
+			expectedFound: true,
+		},
+		{
+			board: sudoku.Board{
+				{1, 2, 3, 4, 5, 6, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 1, 2, 3, 4, 5, 6},
+				{3, 1, 2, 6, 4, 5, 0, 7, 8},
+				{5, 4, 6, 8, 7, 9, 3, 1, 2},
+				{0, 9, 8, 3, 1, 2, 6, 4, 5},
+				{2, 3, 1, 5, 6, 4, 8, 9, 7},
+				{4, 6, 5, 7, 9, 8, 2, 3, 1},
+				{8, 7, 9, 2, 3, 1, 5, 6, 4},
+			},
+			expectedCol:   6,
+			expectedRow:   3,
+			expectedFound: true,
+		},
+		{
+			board: sudoku.Board{
+				{1, 2, 3, 4, 5, 6, 7, 8, 9},
+				{6, 5, 4, 9, 8, 7, 1, 2, 3},
+				{9, 8, 7, 1, 2, 3, 4, 5, 6},
+				{3, 1, 2, 6, 4, 5, 9, 7, 8},
+				{5, 4, 6, 8, 7, 9, 3, 1, 2},
+				{7, 9, 8, 3, 1, 2, 6, 4, 5},
+				{2, 3, 1, 5, 6, 4, 8, 9, 7},
+				{4, 6, 5, 7, 9, 8, 2, 3, 1},
+				{8, 7, 9, 2, 3, 1, 5, 6, 4},
+			},
+			expectedCol:   0,
+			expectedRow:   0,
+			expectedFound: false,
+		},
+	}
+
+	for _, test := range tests {
+		actualCol, actualRow, actualFound := test.board.GetNextEmptyCell()
+
+		if test.expectedFound != actualFound ||
+			test.expectedCol != actualCol ||
+			test.expectedRow != actualRow {
+			t.Errorf("Expected (%v, %v, %v) but got (%v, %v, %v)!",
+				test.expectedCol, test.expectedRow, test.expectedFound,
+				actualCol, actualRow, actualFound)
+		}
+	}
+}
