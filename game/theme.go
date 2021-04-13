95869cb108b62be52e606ab301f7f97c1b9841b3
Serhat S <serhats@tutamail.com>
2021-04-13 23:12:39 +0300

    feat: add default theme

--- game/theme.go
+++ game/theme.go
@@ -2,6 +2,61 @@ package game
 
 import "github.com/serhatscode/sudoku/ui"
 
+func DefaultTheme() Theme {
+	return Theme{
+		Name: "Default",
+		Board: BoardTheme{
+			Cursor: "#f9da75",
+			Border: ui.ColorPair{
+				FG: "#344861",
+				BG: "#ffffff",
+			},
+			Cells: BoardCellColors{
+				Normal: ui.ColorPair{
+					FG: "black",
+					BG: "#ffffff",
+				},
+				Predefined: ui.ColorPair{
+					FG: "#4a90e2",
+					BG: "#ffffff",
+				},
+				Conflict: ui.ColorPair{
+					FG: "#fb3d3f",
+					BG: "#f7cfd6",
+				},
+				Wrong: ui.ColorPair{
+					FG: "#fb3d3f",
+					BG: "#ffffff",
+				},
+			},
+		},
+		Menu: MenuTheme{
+			Color: ui.ColorPair{
+				FG: "#ffffff",
+				BG: "#fb3d3f",
+			},
+			Cursor: ui.ColorPair{
+				FG: "black",
+				BG: "#ffffff",
+			},
+			Box: ui.ColorPair{
+				FG: "#ffffff",
+				BG: "#fb3d3f",
+			},
+		},
+		Warning: WarningTheme{
+			Text: ui.ColorPair{
+				FG: "#ffffff",
+				BG: "#fb3d3f",
+			},
+			Box: ui.ColorPair{
+				FG: "#ffffff",
+				BG: "#fb3d3f",
+			},
+		},
+	}
+}
+
 type Theme struct {
 	Name    string
 	Board   BoardTheme
